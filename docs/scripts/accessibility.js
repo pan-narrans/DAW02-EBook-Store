7d8f13be39b837db034db9256def6676de2bccb9
Kassito <98308251+Kassito@users.noreply.github.com>
2022-11-10 10:00:25 +0100

    Night mode finished

--- docs/scripts/accessibility.js
+++ docs/scripts/accessibility.js
@@ -1,6 +1,11 @@
 // HTML DOM references
 const bigger = document.getElementById("increase_font_size");
 const smaller = document.getElementById("decrease_font_size");
+const night = document.getElementById("modo_noche");
+// HTML DOM references for the night mode in @media
+const articuloOscuro = document.getElementById("oscuro");
+const pOscuro = document.getElementById("letraOscuro");
+var mql = window.matchMedia("(max-device-width: 800px)");
 
 // Functions
 const increaseFontAll = function () {
@@ -11,6 +16,11 @@ const increaseFontAll = function () {
 const decreaseFontAll = function () {
   console.log("Decreasing font size...");
   decreaseFont(document.body);
+}
+
+const modoNoche = function () {
+  console.log("modonoche...");
+  noche(document.body);
 }
 
 const increaseFont = function (element) {
@@ -31,6 +41,23 @@ const decreaseFont = function (element) {
     decreaseFont(element.children[i]);
 }
 
+const noche = function (element){
+  if (element.style.backgroundColor === "rgb(35, 33, 33)") {
+    element.style.backgroundColor = "white";
+    element.style.color = "black";
+    if(mql.matches){
+      articuloOscuro.style.backgroundColor = "white";
+      letraOscuro.style.color = "black";
+    }
+} else {
+    element.style.backgroundColor = "rgb(35, 33, 33)";
+    articuloOscuro.style.backgroundColor = "rgb(35, 33, 33)";
+    letraOscuro.style.color = "white";
+    element.style.color = "white";
+}
+}
+
 // Events
 bigger.addEventListener('click', increaseFontAll);
 smaller.addEventListener('click', decreaseFontAll);
+night.addEventListener('click', modoNoche);
