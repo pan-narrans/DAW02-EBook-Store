36f218e1f3674c2804e738b3c4ff76e0c1049b94
Alex <45456278+pan-narrans@users.noreply.github.com>
2022-12-05 01:19:01 +0100

    Base de datos editada

--- docs/config.php
+++ docs/config.php
@@ -1,7 +1,37 @@
 <?php
 
-// DATABASE
-define('HOSTNAME', 'localhost');
-define('USERNAME', 'ebookadmin');
-define('PASSWORD', 'ebook');
-define('DATABASE', 'ebook');
+// CREDENCIALES BASE DE DATOS
+define('DB_HOSTNAME', 'localhost'  );
+define('DB_USERNAME', 'ebook-admin');
+define('DB_PASSWORD', 'ebook'      );
+define('DB_DATABASE', 'ebook'      );
+
+// TABLAS BASE DE DATOS
+define('DB_TABLA_USUARIOS'   , 'usuarios' );
+define('DB_TABLA_DATOS'      , 'datos'    );
+define('DB_TABLA_CONTRASEÑAS', 'passwords');
+define('DB_TABLA_TIPOS'      , 'tipos'    );
+
+// COLUMNAS TABA TIPOS
+// TODO:
+
+// COLUMNAS TABLA USUARIOS
+define('USER_ID'  , 'id'     );
+define('USER_NICK', 'usuario');
+define('USER_MAIL', 'correo' );
+define('USER_TIPO', 'tipo'   );
+define('USER_FOTO', 'foto'   );
+
+// COLUMNAS TABLA CONTRASEÑAS
+define('PASS_ID'          , 'id_usuario');
+define('PASS_CONTRASEÑA'  , 'password'  );
+
+// COLUMNAS TABLA DATOS
+define('DATOS_ID'         , 'id_usuario'      );
+define('DATOS_NOMBRE'     , 'nombre'          );
+define('DATOS_APELLIDOS'  , 'apellidos'       );
+define('DATOS_GENERO'     , 'genero'          );
+define('DATOS_CUMPLEAÑOS' , 'fecha_nacimiento');
+define('DATOS_DIRECCION'  , 'direccion'       );
+define('DATOS_PAIS'       , 'pais'            );
+define('DATOS_TARJETA'    , 'tarjeta'         );
--- docs/ebook_db.sql
+++ docs/ebook_db.sql
@@ -1,12 +1,12 @@
 -- CREATE USER
-DROP USER IF EXISTS "ebookadmin"@localhost;
-CREATE USER "ebookadmin" @localhost IDENTIFIED BY "ebook";
-GRANT ALL PRIVILEGES ON * . * TO "ebookadmin" @localhost;
+DROP USER IF EXISTS "ebook-admin"@localhost;
+CREATE USER "ebook-admin" @localhost IDENTIFIED BY "ebook";
+GRANT ALL PRIVILEGES ON * . * TO "ebook-admin" @localhost;
 
 -- CREATE DATABASE
 DROP DATABASE IF EXISTS ebook;
-CREATE DATABASE ebook;
-USE ebook;
+CREATE DATABASE         ebook;
+USE                     ebook;
 
 -- CREATE TABLES
 CREATE TABLE tipos(
@@ -16,7 +16,7 @@ CREATE TABLE tipos(
 
 CREATE TABLE usuarios(
   id      INT PRIMARY KEY AUTO_INCREMENT,
-  usuario VARCHAR(20),
+  usuario VARCHAR(50),
   correo  VARCHAR(100) UNIQUE,
   tipo    INT,
   foto    VARCHAR(255)
@@ -30,7 +30,7 @@ CREATE TABLE passwords(
 CREATE TABLE datos(
   id_usuario        INT PRIMARY KEY,
   nombre            VARCHAR(30),
-  apellido          VARCHAR(30),
+  apellidos         VARCHAR(30),
   genero            VARCHAR(100),
   fecha_nacimiento  DATE,
   direccion         VARCHAR(255),
@@ -40,15 +40,15 @@ CREATE TABLE datos(
 
 -- ADD FOREIGN KEYS
 ALTER TABLE usuarios
-	ADD CONSTRAINT USUARIOTIPOSid FOREIGN KEY (tipo) REFERENCES tipos (codigo)
+	ADD CONSTRAINT UsuarioTiposId FOREIGN KEY (tipo) REFERENCES tipos (codigo)
   ON DELETE SET NULL;
     
 ALTER TABLE passwords
-	ADD CONSTRAINT PASSWORDUSUARIOid FOREIGN KEY (id_usuario) REFERENCES usuarios (id)
+	ADD CONSTRAINT PasswordUsuarioId FOREIGN KEY (id_usuario) REFERENCES usuarios (id)
   ON DELETE CASCADE;
     
 ALTER TABLE datos
-	ADD CONSTRAINT DATOSUSUARIOid FOREIGN KEY (id_usuario) REFERENCES usuarios (id)
+	ADD CONSTRAINT DatosUsuarioId FOREIGN KEY (id_usuario) REFERENCES usuarios (id)
   ON DELETE CASCADE;
 
 -- INSERT MOCK DATA
@@ -64,7 +64,7 @@ INSERT INTO usuarios (usuario, correo, tipo, foto) VALUES
 ("Pans_narrans",  "alex@gmail.es",  3, null),
 ("Izan",          "izan@gmail.es",  2, null);
 
-INSERT INTO datos (id_usuario, nombre, apellido, genero, fecha_nacimiento, direccion, pais, tarjeta) VALUES 
+INSERT INTO datos (id_usuario, nombre, apellidos, genero, fecha_nacimiento, direccion, pais, tarjeta) VALUES 
 (1, "Teresa", "Vázquez",  "mujer",  "1990-07-22", "Calle escofina, 12",   "España", null),
 (2, "Pablo",  "Casanova", "hombre", "1989-02-12", "Calle marfil, 45",     "España", null),
 (3, "Alex",   "Pérez",    "hombre", "1995-11-23", "Calle manzana, 09",    "España", null),
