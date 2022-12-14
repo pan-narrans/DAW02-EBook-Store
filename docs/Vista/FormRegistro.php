3ac72261e9825aa7a9ef03a08b7e198564709863
Alex <45456278+pan-narrans@users.noreply.github.com>
2022-12-14 09:31:43 +0100

    Limpiado formulario de registro

--- docs/Vista/FormRegistro.php
+++ docs/Vista/FormRegistro.php
@@ -1,283 +1,121 @@
 <?php
-    require_once("Plantillas/Cabecera.html");
+require_once("Plantillas/Cabecera.html");
 ?>
-<div class="row">
 
-<!-- IMAGE & TEXT -->
-<article id="oscuro"
-            class="col-md-6 col-sm-12 text-center">
-    <img src="Vista/images/Mock Book sin fondo.png"
-        alt="Libro electrónico con el logo, la e en braille seguida de la palabra 'book'."
-        class="img-fluid">
-    <p id="letraOscuro">Regístrate en el formulario y recibe un cupón de 15% de descuento para tu primera compra.</p>
-</article>
 
-<!-- FORM -->
-<article class="col-md-6 col-sm-12">
-    <h1 class="text-center"
-        id="title">REGÍSTRATE</h1>
-    <p class="text-end text-danger">* Campo obligatorio</p>
+<main class="container">
 
-    <form action="index.php"
-        method="POST"
-        id="landingForm"
-        aria-label="Formulario para darse de alta en la tienda.">
+  <div class="row">
 
-    <!-- AVATAR -->
-    <fieldset>
-        <legend class="d-none">Avatar</legend>
-        <label for="avatar" class="d-none">Avatar</label>
-        <input type="file" accept="image/jpeg" id="avatar" name="avatar">
-    </fieldset>
+    <!-- FORM -->
+    <article class="col-lg-8 col-md-10 col-sm-12 mx-auto">
+      <h1 class="text-center" id="title">Nuevo Registro</h1>
 
-    <!-- USERNAME -->
-    <fieldset>
-        <legend class="d-none">Nombre de usuario</legend>
-        <div class=" input-group">
-        <label for="usuario"
-                class="d-none"> Nombre de Usuario (Obligatorio)</label>
-        <input type="usuario"
-                name="usuario"
-                id="usuario"
-                placeholder="Nombre de Usuario"
-                class="form-control w-100"
-                required>
-        <span></span>
-        </div>
+      <form action="index.php" method="POST" id="landingForm" aria-label="Formulario para darse de alta en la tienda.">
 
-        <!-- USERNAME ALERT -->
-        <p id="username_alert"
-            class="text-danger">
-    </fieldset>
+        <!-- USERNAME -->
+        <fieldset>
+          <legend class="d-none">Nombre de usuario</legend>
+          <div class=" input-group">
+            <label for="usuario" class="d-none"> Nombre de Usuario (Obligatorio)</label>
+            <input type="usuario" name="usuario" id="usuario" placeholder="Nombre de Usuario" class="form-control w-100" required>
+            <span></span>
+          </div>
 
-    <!-- NAME & SURNAME -->
-    <fieldset>
-        <legend class="d-none">Nombre y Apellidos</legend>
-        <div class=" input-group">
-        <label for="nombre"
-                class="d-none"> Nombre (Obligatorio)</label>
-        <input type="text"
-                name="nombre"
-                id="nombre"
-                placeholder="Nombre"
-                class="form-control"
-                required>
-        <span></span>
-        <label for="apellidos"
-                class="d-none"> Apellidos (Obligatorio)</label>
-        <input type="text"
-                name="apellidos"
-                id="apellidos"
-                placeholder="Apellidos"
-                class="form-control"
-                required>
-        <span></span>
-        </div>
+          <!-- USERNAME ALERT -->
+          <p id="username_alert" class="text-danger">
+        </fieldset>
 
-        <!-- NAME & SURNAME ALERT -->
-        <p id="nombre_alert"
-            class="text-danger">
-        <p id="apellidos_alert"
-            class="text-danger">
-    </fieldset>
+        <!-- NAME & SURNAME -->
+        <fieldset>
+          <legend class="d-none">Nombre y Apellidos</legend>
+          <div class=" input-group">
+            <label for="nombre" class="d-none"> Nombre (Obligatorio)</label>
+            <input type="text" name="nombre" id="nombre" placeholder="Nombre" class="form-control" required>
+            <span></span>
+            <label for="apellidos" class="d-none"> Apellidos (Obligatorio)</label>
+            <input type="text" name="apellidos" id="apellidos" placeholder="Apellidos" class="form-control" required>
+            <span></span>
+          </div>
 
-    <!-- EMAIL -->
-    <fieldset>
-        <legend class="d-none">E-mail</legend>
-        <div class=" input-group">
-        <label for="email"
-                class="d-none"> E-mail (Obligatorio)</label>
-        <input type="email"
-                name="email"
-                id="email"
-                placeholder="E-mail"
-                class="form-control"
-                required>
-        <span></span>
-        </div>
+          <!-- NAME & SURNAME ALERT -->
+          <p id="nombre_alert" class="text-danger">
+          <p id="apellidos_alert" class="text-danger">
+        </fieldset>
 
-        <!-- NAME & SURNAME ALERT -->
-        <p id="email_alert"
-            class="text-danger">
-    </fieldset>
+        <!-- EMAIL -->
+        <fieldset>
+          <legend class="d-none">E-mail</legend>
+          <div class=" input-group">
+            <label for="email" class="d-none"> E-mail (Obligatorio)</label>
+            <input type="email" name="email" id="email" placeholder="E-mail" class="form-control" required>
+            <span></span>
+          </div>
 
-    <!-- PASSWORD -->
-    <fieldset>
-        <legend class="d-none">Contraseña</legend>
-        <div class=" input-group">
-        <label for="password_1"
-                class="d-none"> Contraseña (Obligatorio)</label>
-        <input type="password"
-                name="password_1"
-                id="password_1"
-                placeholder="Contraseña"
-                class="form-control"
-                required>
-        <span></span>
-        <label for="password_2"
-                class="d-none"> Repetir Contraseña (Obligatorio)</label>
-        <input type="password"
-                name="password_2"
-                id="password_2"
-                placeholder="Repetir Contraseña"
-                class="form-control"
-                required>
-        <span></span>
-        </div>
+          <!-- NAME & SURNAME ALERT -->
+          <p id="email_alert" class="text-danger">
+        </fieldset>
 
-        <!-- PASSWORD ALERTS -->
-        <div id="password_alerts"
-             class="my-3 text-danger"></div>
-        <div id="password_alert_match"
-             class="my-3 text-danger"></div>
-    </fieldset>
+        <!-- PASSWORD -->
+        <fieldset>
+          <legend class="d-none">Contraseña</legend>
+          <div class=" input-group">
+            <label for="password_1" class="d-none"> Contraseña (Obligatorio)</label>
+            <input type="password" name="password_1" id="password_1" placeholder="Contraseña" class="form-control" required>
+            <span></span>
+            <label for="password_2" class="d-none"> Repetir Contraseña (Obligatorio)</label>
+            <input type="password" name="password_2" id="password_2" placeholder="Repetir Contraseña" class="form-control" required>
+            <span></span>
+          </div>
 
-    <!-- GENERO -->
-    <fieldset class="input-group mb-3">
-        <legend class="input-group-text form-control">Género </legend>
-        <div class="input-group-text">
-        <input class="form-check-input mt-0" id="hombre" name="genero" type="radio" value="hombre">
-        </div>
-        <label for="hombre" class="form-control" >Hombre</label>
-        
-        <div class="input-group-text">
-        <input class="form-check-input mt-0" id="mujer" name="genero" type="radio" value="mujer">
-        </div>
-        <label for="mujer" class="form-control" >Mujer</label>
-    </fieldset>
+          <!-- PASSWORD ALERTS -->
+          <div id="password_alerts" class="my-3 text-danger"></div>
+          <div id="password_alert_match" class="my-3 text-danger"></div>
+        </fieldset>
 
-    <!-- EDAD -->
-    <fieldset>
-        <legend class="d-none">Fecha de nacimiento</legend>
-        <div class="input-group">
-        <label for="fecha_nacimiento"
-                class="input-group-text">Cumpleaños: </label>
-        <input type="date"
-                class="form-control"
-                name="fecha_nacimiento"
-                id="fecha_nacimiento">
-        </div>
+        <!-- AVATAR -->
+        <fieldset>
+          <legend class="d-none">Avatar</legend>
+          <div class="input-group">
+            <label for="avatar" class="input-group-text">Avatar</label>
+            <input type="file" accept="image/jpeg" class="form-control" id="avatar" name="avatar">
+          </div>
+          <p></p>
+        </fieldset>
 
-        <!-- EDAD ALERT -->
-        <p id="fecha_alert"
-            class="text-danger">
-    </fieldset>
+        <!-- CHECKBOXES -->
+        <fieldset class="mb-3">
+          <legend class="d-none">Notificaciones y suscripción</legend>
 
-    <!-- DIRECCIÓN -->
-    <fieldset class="mb-3 input-group">
-        <label for="direccion"
-                class="input-group-text">Dirección</label>
-        <legend class="d-none">Dirección</legend>
-        <input type="text"
-                name="direccion"
-                id="direccion"
-                class="form-control">
-        <label for="pais"
-                class="d-none">País</label>
-        <select name="pais"
-                id="pais"
-                class="form-select"
-                placeholder="País"> <!--Cambiar-->
-        </select>
-    </fieldset>
+          <!-- SUSCRIPCIÓN -->
+          <label class="form-check">
+            <input type="checkbox" name="suscripcion" id="suscripcion" checked>
+            Quiero suscribirme y recibir información periódica.
+          </label>
+        </fieldset>
 
-    <!-- TARJETA DE CRÉDITO -->
+        <p class="text-start text-danger">* Campo obligatorio</p>
+        <!-- SUBMIT -->
 
-    <fieldset id="tarjeta_credito">
-        <legend class="d-none">Tarjeta de crédito</legend>
-        <div class="input-group">
-        <legend class="input-group-text">Tarjeta</legend>
-        <label for="tarjeta_num_1" class="d-none">Primeros 4 números</label>
-        <input type="text"
-                name="tarjeta_num_1"
-                id="tarjeta_num_1"
-                maxlength="4"
-                class="form-control">
-        <label for="tarjeta_num_2" class="d-none">Segundos 4 números</label>
-        <input type="text"
-                name="tarjeta_num_2"
-                id="tarjeta_num_2"
-                maxlength="4"
-                class="form-control">
-        <label for="tarjeta_num_3" class="d-none">Terceros 4 números</label>
-        <input type="text"
-                name="tarjeta_num_3"
-                id="tarjeta_num_3"
-                maxlength="4"
-                class="form-control">
-        <label for="tarjeta_num_4" class="d-none">Cuartos 4 números</label>
-        <input type="text"
-                name="tarjeta_num_4"
-                id="tarjeta_num_4"
-                maxlength="4"
-                class="form-control">
-        </div>
+        <section class="d-flex justify-content-between">
+          <button type="submit" aria-label="Enviar formulario" class="btn btn-primary">
+            Enviar formulario</button>
+          <input type="reset" value="Resetear valores" aria-label="Resetear los valores del formulario" class="btn btn-secondary">
+        </section>
 
-        <!-- TARJETA ALERT -->
-        <p id="tarjeta_alert"
-            class="text-danger">
-    </fieldset>
+        <input type="text" name="op" id="op" hidden value="registrar_usuario">
 
-    <!-- CHECKBOXES -->
-    <fieldset class="mb-3">
-        <legend class="d-none">Notificaciones y suscripción</legend>
-        <!-- VENDEDOR -->
-        <label class="form-check">
-        <input type="checkbox"
-                name="vendedor"
-                id="vendedor">
-        Quiero ser vendedor
-        </label>
-        <!-- NOTIFICACIONES -->
-        <label class="form-check">
-        <input type="checkbox"
-                name="notificaciones"
-                id="notificaciones">
-        Activar notificaciones de la pagina
-        </label>
-
-        <!-- SUSCRIPCIÓN -->
-        <label class="form-check">
-        <input type="checkbox"
-                name="suscripcion"
-                id="suscripcion"
-                checked>
-        Quiero suscribirme y recibir información periódica.
-        </label>
-    </fieldset>
-
-    <!-- SUBMIT -->
-
-    <section class="d-flex justify-content-between">
-        <button type="submit"
-                aria-label="Enviar formulario"
-                class="btn btn-primary">
-        Enviar formulario</button>
-
-        <input type="reset"
-               value="Resetear valores"
-               aria-label="Resetear los valores del formulario"
-               class="btn btn-secondary">
-    </section>
-
-    <input type="text"
-           name="op"
-           id="op"
-           hidden
-           value="registrar_usuario">
-
-    <section class="my-3">
-        <p id="submit_alert"
-            class="text-success"></p>
-    </section>
-    </form>
-</article>
-</div>
+        <section class="my-3">
+          <p id="submit_alert" class="text-success"></p>
+        </section>
+      </form>
+    </article>
+  </div>
 </main>
 <footer id="franja-movil"></footer>
 
 <script src="Vista/scripts/landing.js"></script>
 
 <?php
-  require_once("Plantillas/Pie.html");
+require_once("Plantillas/Pie.html");
 ?>
